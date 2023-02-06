c7ca6eaa681334c54542671e79a20707049ab52b
jousethen <jthen93@gmail.com>
2023-02-06 17:39:06 -0500

    Modified Model

--- backend/ggstelo/models.py
+++ backend/ggstelo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+  id=models.CharField(max_length=50)
+  slug=models.SlugField(max_length=50)
+  gamer_tag=models.CharField(max_length=50)
+  elo=models.PositiveIntegerField()
+  highest_elo=models.PositiveIntegerField()
+  created_at=models.DateTimeField(auto_now_add=True)
+  updated_at=models.DateTimeField(auto_now=True)
+  
+  class Meta:
+    ordering=('-gamer_tag',)
+  
+  def __str__(self):
+    return self.gamer_tag
+
