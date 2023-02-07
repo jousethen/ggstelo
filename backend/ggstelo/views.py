88418918339aa27b894f2dcc610a95c32580f3a4
jousethen <jthen93@gmail.com>
2023-02-07 16:08:25 -0500

    Setup views and serializers

--- backend/ggstelo/views.py
+++ backend/ggstelo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Player
+from .serializers import PlayerSerializer
 
 # Create your views here.
+class PlayerListAPIView(generics.ListAPIView):
+  serializer_class=PlayerSerializer
+  
+  def get_queryset(self):
+      return Player.objects.all()
+  
