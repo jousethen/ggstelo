7cbe83effe661bd3c81c2d8ea8108b958603dc18
jousethen <jthen93@gmail.com>
2023-02-08 16:32:05 -0500

    Set up admin portal and api

--- backend/ggstelo/admin.py
+++ backend/ggstelo/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Player
 # Register your models here.
 
-admin.site.register(Player)
+
+class PlayerModelAdmin(admin.ModelAdmin):
+    list_display = ('gamer_tag', 'elo', 'highest_elo',
+                    'created_at', 'updated_at')
+    search_fields = ('gamer_tag', 'slug')
+    list_per_page = 50
+
+
+admin.site.register(Player, PlayerModelAdmin)
--- backend/ggsteloapi/settings.py
+++ backend/ggsteloapi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'ggstelo',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
