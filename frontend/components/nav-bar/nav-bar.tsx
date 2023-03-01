3084fa8b4da4c8fc8a05226200575015f949577a
jousethen <jthen93@gmail.com>
2023-03-01 16:44:25 -0500

    Created NavBar component

---
+++ frontend/components/nav-bar/nav-bar.tsx
@@ -0,0 +1,9 @@
+import type { NavBarProps } from "./interfaces";
+
+export default function NavBar({ }: NavBarProps): JSX.Element {
+  return (
+    <div className="nav-bar">
+      TEST
+    </div>
+  );
+}
--- frontend/components/test/test.tsx
+++
@@ -1,9 +0,0 @@
-import type { TestProps } from "./interfaces";
-
-export default function Test({}: TestProps): JSX.Element {
-  return (
-    <div className="test">
-
-    </div>
-  );
-}
