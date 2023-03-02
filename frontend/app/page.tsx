330e04d3057f347205d860876d0819429813994f
jousethen <jthen93@gmail.com>
2023-03-02 02:40:37 -0500

    basic nav bar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,4 @@
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
 import { Player } from '@/lib/types_be'
 import { routes } from '@/lib/routes'
 const inter = Inter({ subsets: ['latin'] })
@@ -7,11 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default async function Home() {
   const data = await getData()
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get Players
-        </p>
+    <main>
+      <div>
         <ol>
           {data.map((d: Player, index: number) => {
             return <li key={index}>{d.gamer_tag} {d.elo}</li>
--- frontend/components/nav-bar/nav-bar.tsx
+++ frontend/components/nav-bar/nav-bar.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import type { NavBarProps } from "./interfaces";
 
 export default function NavBar({ }: NavBarProps): JSX.Element {
   return (
-    <div className="nav-bar">
-      TEST
-    </div>
+    <nav className="nav-bar">
+      <Link href={"/"}>
+        <h1>SWAY GGST RANKINGS</h1>
+      </Link>
+      <div className="nav-menu-item">
+        <ul>
+          <li> <Link href="/tournaments">Tournaments</Link></li>
+          <li> <Link href="/about">About</Link></li>
+          <li> <Link href="/about">Admin</Link></li>
+        </ul>
+      </div>
+    </nav>
   );
 }
