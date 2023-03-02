549be24f7a9c308cc9f6e3ac7423547fc068c1ea
jousethen <jthen93@gmail.com>
2023-03-02 03:15:25 -0500

    nav bar links added and css modified

--- frontend/components/nav-bar/nav-bar.tsx
+++ frontend/components/nav-bar/nav-bar.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
+import styles from "./nav-bar.module.css"
 import type { NavBarProps } from "./interfaces";
 
 export default function NavBar({ }: NavBarProps): JSX.Element {
   return (
-    <nav className="nav-bar">
+    <nav className={styles["nav-bar"]}>
       <Link href={"/"}>
-        <h1>SWAY GGST RANKINGS</h1>
+        <h1 className="nav-title">SWAY RANKINGS</h1>
       </Link>
-      <div className="nav-menu-item">
-        <ul>
-          <li> <Link href="/tournaments">Tournaments</Link></li>
-          <li> <Link href="/about">About</Link></li>
-          <li> <Link href="/about">Admin</Link></li>
-        </ul>
-      </div>
+      <ul className={styles["nav-menu-items"]}>
+        <li> <Link href="/tournaments">Tournaments</Link></li>
+        <li> <Link href="/about">About</Link></li>
+        <li> <Link href="/about">Admin</Link></li>
+      </ul>
+
+      <button className={styles["nav-menu-dropdown"]}>Icon</button>
     </nav>
   );
 }
