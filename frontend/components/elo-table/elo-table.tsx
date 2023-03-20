3ccaf0185bd1fc509f76749c8eb257d272a454ae
jousethen <jthen93@gmail.com>
2023-03-20 13:52:45 -0400

    Added title to table

--- frontend/components/elo-table/elo-table.tsx
+++ frontend/components/elo-table/elo-table.tsx
@@ -4,35 +4,38 @@ import styles from "./elo-table.module.css"
 
 export default function EloTable({ tableData }: EloTableProps): JSX.Element {
   return (
-    <table className={styles["elo-table"]}>
-      <thead className={styles["header"]}>
-        <tr>
-          <th>
-            Rank
-          </th>
-          <th>
-            Tag
-          </th>
-          <th>
-            ELO
-          </th>
-          <th>
-            Highest ELO
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {tableData.map((d: Player, index: number) => {
-          return (
-            <tr key={index} className={styles["row"]}>
-              <td className={styles["cell"]}>{index + 1}</td>
-              <td className={styles["cell"]}>{d.gamer_tag}</td>
-              <td className={styles["cell"]}>{d.elo}</td>
-              <td className={styles["cell"]}>{d.highest_elo}</td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+    <>
+      <h1 className={styles["table-title"]}>Top 50 GGST Players</h1>
+      <table className={styles["elo-table"]}>
+        <thead className={styles["header"]}>
+          <tr>
+            <th>
+              Rank
+            </th>
+            <th>
+              Tag
+            </th>
+            <th>
+              ELO
+            </th>
+            <th>
+              Highest ELO
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((d: Player, index: number) => {
+            return (
+              <tr key={index} className={styles["row"]}>
+                <td className={styles["cell"]}>{index + 1}</td>
+                <td className={styles["cell"]}>{d.gamer_tag}</td>
+                <td className={styles["cell"]}>{d.elo}</td>
+                <td className={styles["cell"]}>{d.highest_elo}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
